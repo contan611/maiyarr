47fa571e7fdf596b914cf97f6f310737076722f6
contan611 <jbs36_1414@js.hs.kr>
2026-06-05 13:44:47 +0900

    Delete STOP_MAFIA_SERVER.bat

--- STOP_MAFIA_SERVER.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set PORT=8787
-powershell -NoProfile -ExecutionPolicy Bypass -Command "$listeners = Get-NetTCPConnection -LocalPort %PORT% -State Listen -ErrorAction SilentlyContinue; foreach ($l in $listeners) { Stop-Process -Id $l.OwningProcess -Force -ErrorAction SilentlyContinue }"
